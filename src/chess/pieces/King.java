a35cc90da3f3da7932b282473534ebe49ae2ea86
Fabricio Junior <fabriciojosejunior@gmail.com>
2022-12-12 15:30:50 -0300

    Possible moves of a piece

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -14,5 +14,12 @@ public class King extends ChessPiece{
 	public String toString() {
 		return "K";
 	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		// TODO Auto-generated method stub
+		boolean [][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return null;
+	}
 	
 }
